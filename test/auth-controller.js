d7300aca9524b402374889b823f4e8b237e7fdc3
Henning Odén <henning.oden@outlook.com>
2020-12-16 16:36:19 +0100

    474. Testing Code With Active Database & Cleanup

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -1,28 +1,72 @@
-const expect = require('chai').expect;
-const sinon = require('sinon');
+const expect = require("chai").expect;
+const sinon = require("sinon");
+const mongoose = require("mongoose");
 
-const User = require('../models/user');
-const AuthController = require('../controllers/auth');
+const User = require("../models/user");
+const AuthController = require("../controllers/auth");
 
-describe('Auth Controller - login', function() {
-    
-    it('should throw an error if accessing the database fails', function(done) {
-        sinon.stub(User, 'findOne');
-        User.findOne.throws();
+describe("Auth Controller - login", function () {
+  it("should throw an error if accessing the database fails", function (done) {
+    sinon.stub(User, "findOne");
+    User.findOne.throws();
 
-        const req = {
-            body: {
-                email: 'test@test.com',
-                password: 'tester'
-            }
-        };
-        AuthController.login(req, {}, () => {}).then(result => {
-            expect(result).to.be.an('error');
-            expect(result).to.have.property('statusCode', 500);
-            done();
-        }).catch(err => {
-            done(err);
+    const req = {
+      body: {
+        email: "test@test.com",
+        password: "tester",
+      },
+    };
+    AuthController.login(req, {}, () => {})
+      .then((result) => {
+        expect(result).to.be.an("error");
+        expect(result).to.have.property("statusCode", 500);
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+    User.findOne.restore();
+  });
+
+  it("should send a response with a valid user status for an existing user", function (done) {
+    mongoose
+      .connect(require("../util/database"))
+      .then((result) => {
+        const user = new User({
+          email: "test@test.com",
+          password: "tester",
+          name: "Test",
+          posts: [],
+          _id: "5c0f66b979af55031b34728a",
         });
-        User.findOne.restore();
-    })
-})
+        return user.save();
+      })
+      .then(() => {
+        const req = { userId: "5c0f66b979af55031b34728a" };
+        const res = {
+          statusCode: 500,
+          userStatus: null,
+          status: function (code) {
+            this.statusCode = code;
+            return this;
+          },
+          json: function (data) {
+            this.userStatus = data.status;
+          },
+        };
+        AuthController.getUserStatus(req, res, () => {}).then(() => {
+          expect(res.statusCode).to.be.equal(200);
+          expect(res.userStatus).to.be.equal("I am new!");
+          User
+            .deleteMany({})
+            .then(() => {
+              return mongoose.disconnect();
+            })
+            .then(() => {
+              done();
+            });
+        });
+      })
+      .catch((err) => console.log(err));
+  });
+});
